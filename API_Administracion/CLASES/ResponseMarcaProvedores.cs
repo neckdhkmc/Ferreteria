fa7c3f3bcb58d86bad41c6bc5cb84f6b2130b74e
MARCOS JESUS MENDOZA AVILA <marcosjma.2019@gmail.com>
2024-02-09 19:44:09 -0600

    modificacion en capa de datos

---
+++ API_Administracion/CLASES/ResponseMarcaProvedores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Administracion.CLASES
+{
+    public class ResponseMarcaProvedores
+    {
+        public int idProvedor { get; set; }
+
+        public string NombreProvedor { get; set; }
+
+        public List<DatosMarca> ListaNomMarcas { get; set; }
+
+        public int Codigo { get; set; }
+
+        public string Mensaje { get; set; }
+    }
+}
--- API_Administracion/Controllers/Administracion.cs
+++ API_Administracion/Controllers/Administracion.cs
@@ -50,9 +50,78 @@ namespace API_Administracion.Controllers
             return respuesta;          
         
         }
+
+        [HttpPost("ConsultaMarcaProvedor")]
+
+        public ResponseMarcaProvedores ConsultaMarcaPorProbedor(Datosprovedor datos)
+        {
+            ResponseMarcaProvedores response = new ResponseMarcaProvedores();
+            try
+            {
+                Persistencia objpersistence = new Persistencia();
+
+                 response = objpersistence.consultaMarcasPorProvedor(datos);
+
+                if (response.Codigo == 0)
+                {
+                    return response;
+
+
+                } else
+                {
+                    response.Codigo = 1;
+                    response.Mensaje = "no se encontraron registros";
+                
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            return response;
+        }
+
         #endregion
 
         #region marcas
+        [HttpPost("RegistrarMarca")]
+        public ResponseGeneral RegistrarMarca(DatosMarca datos)
+        {
+
+
+            //_logger.LogInformation("Metodo RegistroUsuario inicio");
+
+            ResponseGeneral respuesta = new ResponseGeneral();
+            try
+            {
+                Persistencia objpersistence = new Persistencia();
+
+                var responseRegMarca = objpersistence.regitroMarca(datos);
+                if (responseRegMarca)
+                {
+                    respuesta.codigo = 0;
+                    respuesta.Mensaje = "Marca registrada exitosamente";
+                }
+                else
+                {
+                    respuesta.codigo = 1;
+                    respuesta.Mensaje = "Error al registrar Marca";
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                respuesta.codigo = 1;
+                respuesta.Mensaje = "Error al registrar Marca";
+            }
+
+            return respuesta;
+
+        }
 
         #endregion
         #region ventas 
@@ -60,6 +129,11 @@ namespace API_Administracion.Controllers
         #endregion
 
         #region inventario
+
+        #endregion
+
+        #region Productos
+
 
         #endregion
 
